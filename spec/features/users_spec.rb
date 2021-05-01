f1718319ca71a264afead694c10401076b37ab12
Nasa28 <kaluchinasaagu@gmail.com>
2021-05-01 22:01:21 +0100

    add feature test for unsuccessful sign up

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -13,5 +13,18 @@ RSpec.feature "Users", type: :feature do
       visit root_path
       expect(page).to have_content('Sign out')
     end
+
+    scenario 'unsuccesfully sign up' do
+      visit new_user_registration_path
+      fill_in 'Name', with: user.name
+      fill_in 'Email', with: ''
+      fill_in 'Password', with: '123456'
+      fill_in 'Password confirmation', with: '123456'
+      click_on 'Sign up'
+      visit root_path
+      expect(page).to_not have_content('Sign out')
+    end
   end
+
+  
 end
