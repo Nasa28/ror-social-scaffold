bfbdb2cc60bdacda56676d2cd15a619b5f3b2670
Nasa28 <kaluchinasaagu@gmail.com>
2021-05-01 21:16:15 +0100

    Add test for friendship model associations

---
+++ spec/models/friendship_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Friendship, type: :model do
+  context 'Association' do 
+    it 'Should Belong to User' do
+      friend = Friendship.reflect_on_association(:user).macro
+      expect(friend).to eq(:belongs_to)
+    end
+
+    it 'Should belong to friend' do
+      friend = Friendship.reflect_on_association(:friend).macro
+      expect(friend).to eq(:belongs_to)
+    end
+  end
+end
