8e10c6917b1223330c34214060fed04dbdd2668e
Nasa28 <kaluchinasaagu@gmail.com>
2021-05-01 22:23:34 +0100

    add feature test for user unsuccessful log in

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -36,5 +36,12 @@ RSpec.feature "Users", type: :feature do
       click_on 'Log in'
       expect(page).to have_content('Signed in successfully')
     end
+    scenario 'unsuccesfully log in' do
+      visit new_user_session_path
+      fill_in 'Email', with: ''
+      fill_in 'Password', with: my_user.password
+      click_on 'Log in'
+      expect(page).to have_content('Invalid Email or password')
+    end
   end
 end
