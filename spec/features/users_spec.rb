c9a55b20202f6e4ff60b4e293ee2dbf90699223a
Nasa28 <kaluchinasaagu@gmail.com>
2021-05-01 22:22:33 +0100

    add feature test for user successful sign in

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.feature "Users", type: :feature do
   let(:user) { User.new(name: 'Chinasa', email: 'chinasa@gmail.com', password: '123456') }
+  let(:my_user) { User.create(name: 'Chinasa', email: 'chinasa@gmail.com', password: '123456') }
+
   context 'sign up:' do
     scenario 'sign up succesfully' do
       visit new_user_registration_path
@@ -18,13 +20,21 @@ RSpec.feature "Users", type: :feature do
       visit new_user_registration_path
       fill_in 'Name', with: user.name
       fill_in 'Email', with: ''
-      fill_in 'Password', with: '123456'
-      fill_in 'Password confirmation', with: '123456'
+      fill_in 'Password', with: user.password
+      fill_in 'Password confirmation', with: user.password
       click_on 'Sign up'
       visit root_path
       expect(page).to_not have_content('Sign out')
     end
   end
 
-  
+  context 'loging in:' do
+    scenario 'succesfully log in' do
+      visit new_user_session_path
+      fill_in 'Email', with: my_user.email
+      fill_in 'Password', with: my_user.password
+      click_on 'Log in'
+      expect(page).to have_content('Signed in successfully')
+    end
+  end
 end
