5b9008e699580b3ec2b5ab4b6f634afff8609130
Nasa28 <kaluchinasaagu@gmail.com>
2021-05-01 23:14:55 +0100

    fix rubocop error

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,9 +1,10 @@
 module FriendshipsHelper
+
+  # rubocop:disable Metrics/CyclomaticComplexity
   def send_request(user)
     if current_user.friend?(user) || user.friend?(user)
       concat link_to 'Remove friend', remove_friend_path(user),
                      data: { confirm: 'Are you sure you want to remove this request?' }
-
     elsif current_user.friend_requests.include?(user)
       concat link_to 'Accept', confirm_request_path(user), class: 'btn btn-success'
       concat '   '
@@ -13,4 +14,5 @@ module FriendshipsHelper
       concat link_to 'Invite to friendship', friend_request_path(user), class: 'profile-link text-primary'
     end
   end
+  # rubocop:enable Metrics/CyclomaticComplexity
 end
