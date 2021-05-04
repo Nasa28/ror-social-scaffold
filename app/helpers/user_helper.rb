97c7d4cf21e6765eae358fb40e0d9a44c1863667
Nasa28 <kaluchinasaagu@gmail.com>
2021-05-04 23:50:35 +0100

    move alert partial to helper

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,20 @@
 module UserHelper
+  def alert_me
+    result = ' '
+    if notice.present? 
+    result += '<div class="notice">'
+       result += '<p>'
+      result += notice 
+       result += '</p>'
+      result += '</div>'
+    end 
+   if alert.present?
+     result += '<div class="alert">'
+     '<p>'
+     result +=  alert 
+     '</p>'
+     result += '</div>'
+    end
+    result.html_safe
+ end
 end
